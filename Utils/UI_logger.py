1deb089084f26f3a33024b9f5fa5050bcc404ca4
Guilherme Vinicius Pedroso Pedroza <bherewow@gmail.com>
2023-10-09 15:49:14 -0300

    requirements

--- Utils/UI_logger.py
+++ Utils/UI_logger.py
@@ -1,15 +1,4 @@
-import tkinter as tk
 import io
-import sys
-
-
-class OutputRedirector(io.TextIOBase):
-    def __init__(self, text_widget):
-        self.text_widget = text_widget
-
-    def write(self, string):
-        self.text_widget.insert(tk.END, string)
-        self.text_widget.see(tk.END)  # Scroll to the end
 
 
 class OutputRedirectorQt(io.TextIOBase):
